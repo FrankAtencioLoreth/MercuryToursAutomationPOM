5e37363f9edd5552001d36c8d6fe4d67beeaf297
Frank Atencio <frank.atencio.loreth@gmail.com>
2022-12-24 14:09:52 -0500

    Cambios para agregar un tiempo de retraso a cada paso (en segundos)

--- src/main/java/Pages/BasePage.java
+++ src/main/java/Pages/BasePage.java
@@ -2,17 +2,21 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class BasePage {
 
+    private Properties conf;
     private final WebDriver driver;
-    private final int time = 20;
+    private Integer time = 20;
+    private Integer d = 0;
+
     public BasePage(WebDriver driver) {
         this.driver = driver;
     }
@@ -24,11 +28,13 @@ public class BasePage {
     }
 
     public void click(By locator) {
+        this.delay(this.d);
         WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(this.time));
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).click();
     }
 
     public void sendKeys(By locator, String text) {
+        this.delay(this.d);
         WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(this.time));
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(text);
     }
@@ -49,11 +55,20 @@ public class BasePage {
     }
 
     public void selectItemByText(By locator, String text) {
+        this.delay(this.d);
         WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(this.time));
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(locator));
         Select select = new Select(this.driver.findElement(locator));
         select.selectByVisibleText(text);
     }
 
+    private void delay(int d) {
+        try {
+            TimeUnit.SECONDS.sleep(d);
+        }catch (InterruptedException e) {
+            System.err.println("ERROR MSG = " + e.getMessage());
+            System.err.println("ERROR LINE = " + e.getLocalizedMessage());
+        }
+    }
 
 }
--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -9,11 +9,12 @@ public class BaseTest {
 
     public final String URL = "https://demo.guru99.com/test/newtours/index.php";
     public WebDriver driver;
+    private final Integer time = 20;
 
     public BaseTest() {
         this.driver = new ChromeDriver();
         this.driver.manage().window().maximize();
-        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(this.time));
         this.driver.get(this.URL);
     }
 
